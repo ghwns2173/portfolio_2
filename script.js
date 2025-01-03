3c9437c51692d6d668a049fa0b9586481eeeb7f6
ghwns2173 <ghwjd2173@naver.com>
2025-01-03 15:53:24 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener(`DOMContentLoaded`, function () {
+  AOS.init();
+  
+  
+  var swiper = new Swiper(".portfolioSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    });
+  
+  window.addEventListener(`scroll`, function () {
+    const headerBar = document.querySelector(`.header_area`);
+    const windowData = window.scrollY;
+
+    if (windowData <= 0 ) {
+      headerBar.classList.remove(`scroll`);
+    } else {
+      headerBar.classList.add(`scroll`);
+    }
+  });
+  
+}); // end
