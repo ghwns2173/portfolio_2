75eb59cc02d2fddc326a7ed81dca0725d97b753b
ghwns2173 <ghwjd2173@naver.com>
2025-01-15 20:02:39 +0900

    seventh commit

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener(`DOMContentLoaded`, function () {
   
   
   var swiper = new Swiper(".portfolioSwiper", {
+    loop:true,
     slidesPerView: 3,
     spaceBetween: 20,
     navigation: {
